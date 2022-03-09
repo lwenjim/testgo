87ab0ca5930b8a3e43cec7d719a9609526b945a5
lwenjim <779772852@qq.com>
2022-03-09 22:54:39 +0800

    1

--- exception/demo001.go
+++ exception/demo001.go
@@ -11,6 +11,7 @@ func function1() {
 	defer function2()
 	fmt.Printf("In function1 at the bottom!\n")
 }
+
 func function2() {
 	fmt.Printf("function2: Deferred until the end of the calling method!")
 }
