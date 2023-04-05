c5224f4d78c7519f1c866d5ca1618016abf86cea
lwenjim <liuwenjim@gmail.com>
2023-04-05 13:13:46 +0800

    feat: add counter

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
 import (
-	_ "github.com/go-sql-driver/mysql"
+	"fmt"
+	"sync"
+	"time"
 )
 
 func main() {
+	var num int32
+	var w sync.WaitGroup
+	start := time.Now()
+	for i := 0; i < 100000; i++ {
+		num++
+		fmt.Printf("num: %v\n", num)
+	}
+	w.Wait()
+	fmt.Printf("%+v\n", time.Since(start).Seconds())
+	fmt.Printf("%+v\n", "done")
 }
