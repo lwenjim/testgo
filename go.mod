0f3dce20aedef2f3bf50b0f17e7ec4ac72c17a79
lwenjim <liuwenjim@gmail.com>
2023-04-14 23:00:27 +0800

    feat: add dependent

--- go.mod
+++ go.mod
@@ -2,6 +2,10 @@ module testgo
 
 go 1.19
 
-require github.com/go-sql-driver/mysql v1.4.0
+require github.com/google/uuid v1.3.0
 
-require google.golang.org/appengine v1.2.0 // indirect
+require (
+	github.com/stretchr/commander v0.0.0-20130925004555-18f86d1c1da1 // indirect
+	github.com/stretchr/gorc v0.0.0-20130924145046-6dc9c25595e6 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,21 +2,33 @@ package main
 
 import (
 	"fmt"
-	"sync"
-	"sync/atomic"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
-	var n int32
-	var wg sync.WaitGroup
-	for i := 0; i < 1000; i++ {
-		wg.Add(1)
-		go func() {
-			atomic.AddInt32(&n, 1)
-			wg.Done()
-		}()
-	}
-	wg.Wait()
 
-	fmt.Println(atomic.LoadInt32(&n)) // 1000
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+	fmt.Println(os.Getpid())
+	// `signal.Notify` registers the given channel to
+	// receive notifications of the specified signals.
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM, syscall.SIGUSR1, syscall.SIGUSR2)
+	// This goroutine executes a blocking receive for
+	// signals. When it gets one it'll print it out
+	// and then notify the program that it can finish.
+	go func() {
+		sig := <-sigs
+		fmt.Println()
+		fmt.Println(sig)
+		done <- true
+	}()
+	// The program will wait here until it gets the
+	// expected signal (as indicated by the goroutine
+	// above sending a value on `done`) and then exit.
+	fmt.Println("awaiting signal")
+	<-done
+	fmt.Println("exiting")
+
 }
