5d2494f25c0a3f4f739c3f66224dd2adbd88dd4f
lwenjim <liuwenjim@gmail.com>
2023-04-24 14:15:22 +0800

    feat: update main.go

--- k8s/main.go
+++ k8s/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
+	"time"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
@@ -10,6 +12,18 @@ func hello(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	started := time.Now()
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		duration := time.Since(started)
+		if duration.Seconds() > 15 {
+			w.WriteHeader(500)
+			w.Write([]byte(fmt.Sprintf("error: %v", duration.Seconds())))
+		} else {
+			w.WriteHeader(200)
+			w.Write([]byte("ok"))
+		}
+	})
+
 	http.HandleFunc("/", hello)
 	http.ListenAndServe(":3000", nil)
 }
