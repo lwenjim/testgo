3bb69cfa5a3d09b8d00be83b42b6e3cb2f211175
lwenjim <liuwenjim@gmail.com>
2023-04-25 13:34:00 +0800

    feat: ingress

--- k8s/main.go
+++ k8s/main.go
@@ -1,19 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
+	"os"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "[v2] Hello, Kubernetes!")
+	host, _ := os.Hostname()
+	io.WriteString(w, fmt.Sprintf("[v3] Hello, Kubernetes!, From host: %s", host))
 }
 
 func main() {
-	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(500)
-	})
-
 	http.HandleFunc("/", hello)
 	http.ListenAndServe(":3000", nil)
 }
