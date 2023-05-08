c790a9943e941f5130d6d9b279eac92cb89343bb
lwenjim <liuwenjim@gmail.com>
2023-05-08 10:36:34 +0800

    feat: analyze database/sql

--- go.mod
+++ go.mod
@@ -3,7 +3,11 @@ module testgo
 go 1.16
 
 require (
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/jmoiron/sqlx v1.3.5
+	github.com/lib/pq v1.10.9
 	github.com/peterhellberg/giphy v0.0.2
+	github.com/stretchr/testify v1.8.1
 	k8s.io/api v0.27.1
 	k8s.io/client-go v0.27.1
 	k8s.io/kubectl v0.27.1
--- main.go
+++ main.go
@@ -1,23 +1,7 @@
 package main
 
-import (
-	"bytes"
-	"io"
-	"net/http"
-)
+import "fmt"
 
 func main() {
-	buf := bytes.NewBufferString("")
-	resp, err := http.Post("http://www.baidu.com", "application/json", buf)
-	if err != nil {
-		println(err)
-	} else {
-		defer resp.Body.Close()
-		buff, err := io.ReadAll(resp.Body)
-		if err != nil {
-			println(err)
-		} else {
-			println(string(buff))
-		}
-	}
+	fmt.Println(123)
 }
