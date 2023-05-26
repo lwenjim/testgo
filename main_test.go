aa125e381f0c61e4ba0066300d225a7a66f4f95f
lwenjim <liuwenjim@gmail.com>
2023-05-26 17:32:41 +0800

    refactor: add tool.sh

--- main_test.go
+++ main_test.go
@@ -27,19 +27,12 @@ import (
 	OrmMysql "gorm.io/driver/mysql"
 	"gorm.io/gorm"
 
-	"github.com/qustavo/sqlhooks/v2"
-	"k8s.io/client-go/informers"
-
 	"github.com/alicebob/miniredis/v2"
-	"github.com/go-sql-driver/mysql"
-	"github.com/jmoiron/sqlx"
 	"github.com/peterhellberg/giphy"
 	"github.com/redis/go-redis/v9"
 	"github.com/stretchr/testify/assert"
 	v1 "k8s.io/api/core/v1"
-	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/kubectl/pkg/scheme"
 
@@ -58,15 +51,11 @@ func TestGighy(t *testing.T) {
 	g := giphy.DefaultClient
 	g.APIKey = "xVXd8j7UxP8Lvn8Dn1aLjLAd5EHYGE31"
 	g.Rating = "pg-13"
-	g.Limit = 30 * 2
+	g.Limit = 2
 	trendings, _ := g.Search([]string{"11"})
 	for _, trending := range trendings.Data {
 		fmt.Println(trending.MediaURL())
 	}
-
-}
-
-func TestClientSet(t *testing.T) {
 
 }
 
@@ -93,38 +82,6 @@ func TestRestfullClient(t *testing.T) {
 	}
 }
 
-func TestSharedInformerFactory(t *testing.T) {
-	config, err := clientcmd.BuildConfigFromFlags("", clientcmd.RecommendedHomeFile)
-	if err != nil {
-		panic(err)
-	}
-	clientSet, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err)
-	}
-
-	factory := informers.NewSharedInformerFactoryWithOptions(clientSet, 0, informers.WithNamespace("default"))
-	informer := factory.Core().V1().Pods().Informer()
-
-	_, err = informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc: func(obj interface{}) {
-			fmt.Println("Add Event")
-		},
-		UpdateFunc: func(oldObj, newObj interface{}) {
-			fmt.Println("Update Event")
-		},
-		DeleteFunc: func(obj interface{}) {
-			fmt.Println("Delete Event")
-		},
-	})
-	assert.Nil(t, err)
-
-	stopCh := make(chan struct{})
-	factory.Start(stopCh)
-	factory.WaitForCacheSync(stopCh)
-	<-stopCh
-}
-
 func TestGeneralSql(t *testing.T) {
 	lMap := map[string]uint8{
 		"短信":   0,
@@ -148,22 +105,6 @@ func TestGeneralSql(t *testing.T) {
 }
 
 func TestSql(t *testing.T) {
-	/**
-	DROP TABLE IF EXISTS `user`;
-		CREATE TABLE `user` (
-		  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT '编号',
-		  `username` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '用户名',
-		  `password` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '密码',
-		  `mark` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '备注',
-		  `created_time` timestamp(3) NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '创建时间',
-		  `updated_time` timestamp(3) NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT '修改时间',
-		  PRIMARY KEY (`id`) COMMENT '主键索引',
-		  UNIQUE KEY `password` (`password`) USING BTREE COMMENT '唯一索引',
-		  KEY `username` (`username`) USING BTREE COMMENT '普通索引',
-		  FULLTEXT KEY `ftext` (`mark`) COMMENT '全文索引'
-		) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-	*/
-
 	db, err := sql.Open("mysql", "root@tcp(127.0.0.1:33060)/test")
 	assert.Nil(t, err)
 
@@ -284,166 +225,6 @@ func getFreePort() (port int, err error) {
 		}
 	}
 	return
-}
-
-func TestMockMysql(t *testing.T) {
-	dbName := RandStringRunes(10)
-	engine := sqle.NewDefault(ssql.NewDatabaseProvider(
-		memory.NewDatabase(dbName),
-		information_schema.NewInformationSchemaDatabase(),
-	))
-	port, err := getFreePort()
-	assert.Nil(t, err)
-	config := server.Config{
-		Protocol: "tcp",
-		Address:  fmt.Sprintf("localhost:%d", port),
-	}
-	s, err := server.NewDefaultServer(config, engine)
-	assert.Nil(t, err)
-
-	go func() {
-		t2 := s.Start()
-		assert.Nil(t, t2)
-	}()
-	sql.Register("mysqlWithHooks", sqlhooks.Wrap(&mysql.MySQLDriver{}, &Hooks{}))
-	assert.Nil(t, err)
-
-	dsn := fmt.Sprintf("root@tcp(localhost:%d)/%s", port, dbName)
-	db3, _ := sql.Open("mysqlWithHooks", dsn)
-	result, err := db3.Exec(`
-		CREATE TABLE t_user(
-		  id int unsigned NOT NULL AUTO_INCREMENT COMMENT '编号',
-		  user_name varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '用户名',
-		  pass_word varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '密码',
-		  mark varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '备注',
-		  created_time timestamp(3) NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '创建时间',
-		  updated_time timestamp(3) NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT '修改时间',
-		  PRIMARY KEY (id) COMMENT '主键索引',
-		  UNIQUE KEY pass_word (pass_word) USING BTREE COMMENT '唯一索引',
-		  KEY user_name (user_name) USING BTREE COMMENT '普通索引'
-		) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;	
-	`)
-	assert.Nil(t, err)
-
-	_, err = result.RowsAffected()
-	assert.Nil(t, err)
-
-	dbhelp, err := sqlx.Connect("mysql", dsn)
-	assert.Nil(t, err)
-	var data []struct {
-		Id       sql.NullInt64  `json:"id"`
-		UserName sql.NullString `json:"user_name,omitempty" db:"user_name"`
-		Password sql.NullString `json:"pass_word,omitempty"  db:"pass_word"`
-		Mark     sql.NullString `json:"mark,omitempty"`
-	}
-	err = dbhelp.Select(&data, "select id, user_name, pass_word, mark from t_user")
-	assert.Nil(t, err)
-
-	for index := range data {
-		fmt.Printf("id: %v\n", data[index].Id.Int64)
-		fmt.Printf("username: %v\n", data[index].UserName.String)
-		fmt.Printf("password: %v\n", data[index].Password.String)
-		fmt.Printf("mark: %v\n\n", data[index].Mark.String)
-	}
-
-	var schema = `
-		CREATE TABLE person (
-			first_name text,
-			last_name text,
-			email text
-		);
-		
-		CREATE TABLE place (
-			country text,
-			city text NULL,
-			telcode integer
-		)`
-
-	type Person struct {
-		FirstName string `db:"first_name"`
-		LastName  string `db:"last_name"`
-		Email     string
-	}
-
-	type Place struct {
-		Country string
-		City    sql.NullString
-		TelCode int
-	}
-
-	db, err := sqlx.Connect("mysqlWithHooks", dsn)
-	assert.Nil(t, err)
-
-	for _, s := range strings.Split(schema, ";") {
-		db.MustExec(s)
-	}
-
-	tx := db.MustBegin()
-	tx.MustExec("INSERT INTO person (first_name, last_name, email) VALUES (?, ?, ?)", "Jason", "Moiron", "jmoiron@jmoiron.net")
-	tx.MustExec("INSERT INTO person (first_name, last_name, email) VALUES (?, ?, ?)", "John", "Doe", "johndoeDNE@gmail.net")
-	tx.MustExec("INSERT INTO place (country, city, telcode) VALUES (?, ?, ?)", "United States", "New York", "1")
-	tx.MustExec("INSERT INTO place (country, telcode) VALUES (?, ?)", "Hong Kong", "852")
-	tx.MustExec("INSERT INTO place (country, telcode) VALUES (?, ?)", "Singapore", "65")
-	_, _ = tx.NamedExec("INSERT INTO person (first_name, last_name, email) VALUES (:first_name, :last_name, :email)", &Person{"Jane", "Citizen", "jane.citzen@example.com"})
-	_ = tx.Commit()
-
-	people := []Person{}
-	_ = db.Select(&people, "SELECT * FROM person ORDER BY first_name ASC")
-	jason, john := people[0], people[1]
-	fmt.Printf("%#v\n%#v\n", jason, john)
-
-	jason = Person{}
-	err = db.Get(&jason, "SELECT * FROM person WHERE first_name=$1", "Jason")
-	assert.Nil(t, err)
-	fmt.Printf("%#v\n", jason)
-
-	places := []Place{}
-	err = db.Select(&places, "SELECT * FROM place ORDER BY telcode ASC")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	usa, singsing, honkers := places[0], places[1], places[2]
-	fmt.Printf("%#v\n%#v\n%#v\n", usa, singsing, honkers)
-
-	place := Place{}
-	rows, err := db.Queryx("SELECT * FROM place")
-	assert.Nil(t, err)
-	for rows.Next() {
-		err := rows.StructScan(&place)
-		assert.Nil(t, err)
-		fmt.Printf("%#v\n", place)
-	}
-
-	_, err = db.NamedExec(`INSERT INTO person (first_name,last_name,email) VALUES (:first,:last,:email)`, map[string]interface{}{
-		"first": "Bin",
-		"last":  "Smuth",
-		"email": "bensmith@allblacks.nz",
-	})
-	assert.Nil(t, err)
-
-	_, err = db.NamedQuery(`SELECT * FROM person WHERE first_name=:fn`, map[string]interface{}{"fn": "Bin"})
-	assert.Nil(t, err)
-
-	_, err = db.NamedQuery(`SELECT * FROM person WHERE first_name=:first_name`, jason)
-	assert.Nil(t, err)
-
-	personStructs := []Person{
-		{FirstName: "Ardie", LastName: "Savea", Email: "asavea@ab.co.nz"},
-		{FirstName: "Sonny Bill", LastName: "Williams", Email: "sbw@ab.co.nz"},
-		{FirstName: "Ngani", LastName: "Laumape", Email: "nlaumape@ab.co.nz"},
-	}
-	_, err = db.NamedExec(`INSERT INTO person (first_name, last_name, email) VALUES (:first_name, :last_name, :email)`, personStructs)
-	assert.Nil(t, err)
-
-	personMaps := []map[string]interface{}{
-		{"first_name": "Ardie", "last_name": "Savea", "email": "asavea@ab.co.nz"},
-		{"first_name": "Sonny Bill", "last_name": "Williams", "email": "sbw@ab.co.nz"},
-		{"first_name": "Ngani", "last_name": "Laumape", "email": "nlaumape@ab.co.nz"},
-	}
-	_, err = db.NamedExec(`INSERT INTO person (first_name, last_name, email) VALUES (:first_name, :last_name, :email)`, personMaps)
-	assert.Nil(t, err)
-
 }
 
 func TestJson(t *testing.T) {
