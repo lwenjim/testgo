088e3b4c9d7f84bbed8dc6a6a11260dcdf6f0eec
lwenjim <liuwenjim@gmail.com>
2023-07-26 09:11:11 +0800

    feat: time

--- main.go
+++ main.go
@@ -6,22 +6,15 @@ import (
 )
 
 func main() {
-
-	var i int = 0
-	go func() {
-		for {
-			i++
-			fmt.Println("subroutine: i = ", i)
-			time.Sleep(1 * time.Second)
-		}
-	}()
-	for {
-		i++
-		fmt.Println("mainroutine: i = ", i)
-		time.Sleep(1 * time.Second)
+	loc, err := time.LoadLocation("GMT-8")
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
-}
+	fmt.Println(time.Now().Location())
+	fmt.Println(time.Now().Local().Location())
+	fmt.Println(time.Now().UTC().Location())
+	fmt.Println(loc)
 
-func Foo() {
-	println(123)
+	fmt.Println(time.Now().In(loc).Format("2006-01-02 15:04:05"))
 }
