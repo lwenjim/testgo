7f8555f2e5cae4e1b2c908085fc42668167f14c4
lwenjim <liuwenjim@gmail.com>
2023-08-24 17:27:08 +0800

    feat: TestHour

--- main.go
+++ main.go
@@ -1,28 +1,10 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
+import "fmt"
 
 func main() {
-	h := make(handler)
-	go counter(h)
-	if err := http.ListenAndServe(":8000", h); err != nil {
-		log.Print(err)
-	}
-}
-
-func counter(ch chan<- int) {
-	for n := 0; ; n++ {
-		ch <- n
-	}
-}
-
-type handler chan int
-
-func (h handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-type", "text/plain")
-	fmt.Fprintf(w, "%s: you are visitor #%d", req.URL, <-h)
+	a := 1
+	b := 2
+	fmt.Printf("b: %v\n", b)
+	fmt.Printf("a: %v\n", a)
 }
