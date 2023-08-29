b1921d5fef9a8c39b6ef939007fff969eaab25a0
lwenjim <liuwenjim@gmail.com>
2023-08-29 17:01:57 +0800

    feat: change serviceServers to map

--- shell/test.sh
+++ shell/test.sh
@@ -1,53 +1,42 @@
 #! /bin/bash
-# shellcheck disable=SC2086
-mapping="
-        usersv
-        mongo
-        mysql
-        redis
-        pusher
-        messagesv
-        squaresv 
-        edgesv
-        authsv
-        uploadsv
-        deliversv
-        usergrowthsv
-        riskcontrolsv
-        paysv"
-service="$2"
-my_function() {
-    arr=()
-    index=0
-    while test $# -gt 0; do
-        if [ "$service" = "$1" ]; then
-            arr[index]=$1
-            break
-        fi
-        shift
-    done
-    if [ ${#arr[@]} -eq 0 ]; then
-        len=${#service}
-        for ((i = 0; i < ${#service}; i++)); do
-            current=$(echo "$service" | cut -c 1-"$len")
-            for2() {
-                while test $# -gt 0; do
-                    item=$(echo "$1" | cut -c 1-"$len")
-                    if [ "$current" = "$item" ]; then
-                        arr[index]=$1
-                        index=$((index + 1))
-                    fi
-                    shift
-                done
-            }
-            for2 $mapping
-            if [ ${#arr[@]} -gt 0 ]; then
-                break
-            fi
-        done
-    fi
-    for i in "${arr[@]}"; do
-        echo "$i"
-    done
-}
-my_function $mapping
+# shellcheck disable=2086,2068
+
+serviceServers="
+        mongo 27017
+        mysql 3306
+        redis 6379
+        pusher 64440
+        messagesv 64441
+        squaresv 64442
+        edgesv 64443
+        usersv 64444
+
+        authsv 64445
+        uploadsv 64446
+        deliversv 64447
+        usergrowthsv 64448
+        riskcontrolsv 64449
+        paysv 64450
+        connectorsv 64451
+        "
+declare -A serviceServers=(
+    ["mongo"]=27017
+    ["mysql"]=3306
+    ["redis"]=6379
+    ["pusher"]=64440
+    ["messagesv"]=64441
+    ["squaresv"]=64442
+    ["edgesv"]=64443
+    ["usersv"]=64444
+
+    ["authsv"]=64445
+    ["uploadsv"]=64446
+    ["deliversv"]=64447
+    ["usergrowthsv"]=64448
+    ["riskcontrolsv"]=64449
+    ["paysv"]=64450
+    ["connectorsv"]=64451
+)
+for variable in ${!serviceServers[@]}; do
+    echo "$variable"
+done
