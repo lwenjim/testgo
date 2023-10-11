5214959fa80b253fbc0cf4cc52e7e88f301b02ef
lwenjim <liuwenjim@gmail.com>
2023-10-11 22:56:18 +0800

    feat: add TestGrpc

--- main.go
+++ main.go
@@ -1,56 +1,11 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"time"
-
-	"code.jspp.com/jspp/internal-tools/rpc"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/balancer/roundrobin"
-	"google.golang.org/grpc/credentials/insecure"
+	_ "github.com/lwenjim/testgo/a"
+	_ "github.com/lwenjim/testgo/b"
 )
 
 func main() {
-	conn, err := grpc.Dial("127.0.0.1:39090", grpc.WithDefaultServiceConfig(fmt.Sprintf(`{"LoadBalancingPolicy": "%s"}`, roundrobin.Name)), grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	messageClient := rpc.NewMessageClient(conn)
-	resp, err := messageClient.SetTimingRemind(context.Background(), &rpc.SetTimingRemindRequest{
-		ExecuteTime: time.Now().Unix() + 30*int64(time.Minute),
-		MessageItem: &rpc.MessageItem{
-			Content: &rpc.MessageContent{
-				Text: &rpc.TextMessage{
-					Text:            "123",
-					Markdown:        false,
-					IsTimingMessage: false,
-				},
-			},
-		},
-	})
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf("%#v", resp.Response.Result)
-
-	resp2, err := messageClient.GetTimingReminds(context.TODO(), &rpc.GetTimingRemindsRequest{
-		Auth: &rpc.BaseRequest{
-			TokenRaw: []byte{},
-			Token: &rpc.Token{
-				UserId: 110,
-			},
-			RequestId: "",
-		},
-	})
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf("%#v", resp2)
-	for _, timingRemind := range resp2.Items {
-		fmt.Printf("timingRemind.Content: %v\n", timingRemind.Id)
-	}
+	// a.Say()
+	// b.Say()
 }
--- main_test.go
+++ main_test.go
@@ -22,8 +22,12 @@ import (
 	"testing"
 	"time"
 
+	"code.jspp.com/jspp/internal-tools/rpc"
 	"github.com/bitly/go-simplejson"
 	"github.com/google/go-querystring/query"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/balancer/roundrobin"
+	"google.golang.org/grpc/credentials/insecure"
 	"gopkg.in/validator.v2"
 
 	"github.com/alicebob/miniredis/v2"
@@ -451,4 +455,48 @@ func TestSss(t *testing.T) {
 	// 	fmt.Printf("a: %v\n", a)
 	// }()
 	// panic(nil)
+}
+
+func TestYes() {
+	conn, err := grpc.Dial("127.0.0.1:39090", grpc.WithDefaultServiceConfig(fmt.Sprintf(`{"LoadBalancingPolicy": "%s"}`, roundrobin.Name)), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	messageClient := rpc.NewMessageClient(conn)
+	resp, err := messageClient.SetTimingRemind(context.Background(), &rpc.SetTimingRemindRequest{
+		ExecuteTime: time.Now().Unix() + 30*int64(time.Minute),
+		MessageItem: &rpc.MessageItem{
+			Content: &rpc.MessageContent{
+				Text: &rpc.TextMessage{
+					Text:            "123",
+					Markdown:        false,
+					IsTimingMessage: false,
+				},
+			},
+		},
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("%#v", resp.Response.Result)
+
+	resp2, err := messageClient.GetTimingReminds(context.TODO(), &rpc.GetTimingRemindsRequest{
+		Auth: &rpc.BaseRequest{
+			TokenRaw: []byte{},
+			Token: &rpc.Token{
+				UserId: 110,
+			},
+			RequestId: "",
+		},
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("%#v", resp2)
+	for _, timingRemind := range resp2.Items {
+		fmt.Printf("timingRemind.Content: %v\n", timingRemind.Id)
+	}
 }
