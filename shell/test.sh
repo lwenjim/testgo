9dc66b07af687d1b666ae41b8f1494207d3cc215
lwenjim <liuwenjim@gmail.com>
2023-12-09 21:11:06 +0800

    feat: add func print-env-go

--- shell/test.sh
+++ shell/test.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
-# shellcheck disable=SC2206 disable=SC2068 disable=SC2086
+# shellcheck disable=SC2206 disable=SC2068 disable=SC2086 disable=SC1091 disable=SC2317 disable=SC1090 disable=SC2090 disable=SC2089 disable=SC2059
 
-if abc >/dev/null 2>&1; then
-    echo ok
-else
-    echo "a$?a"
-fi
+IFS=$'\n'
+a=$(env)
+b=($a)
+template="%-40s %-10s\n"
+printf ${template} "环境变量" "变量值"
+for variable in ${b[@]}; do
+    IFS="="
+    item=($variable)
+    if [ "${item[0]}" = "PATH" ] || [ "${item[1]}" = "" ]; then
+        continue
+    fi
+    printf ${template} "${item[0]}" "${item[1]}"
+done
+
+# a="a=b"
+# IFS="="
+# item=($a)
+# for variable in ${item[@]}; do
+#     echo $variable
+# done
