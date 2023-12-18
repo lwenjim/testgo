54dabae98df150d8b1ad233c3e445226644c498b
lwenjim <liuwenjim@gmail.com>
2023-12-18 15:08:15 +0800

    feat: format port-forward

--- main.go
+++ main.go
@@ -1,25 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"math/big"
-	"time"
-)
+import "fmt"
 
 func main() {
-	allInputs := [5]int{10000, 50000, 100000, 500000, 1000000}
-	for i := 0; i < len(allInputs); i++ {
-		var before = GetTimeStamp()
-		var factorial *big.Int = big.NewInt(1)
-		for j := 1; j <= allInputs[i]; j++ {
-			factorial = factorial.Mul(factorial, big.NewInt(int64(j)))
-		}
-		var after = GetTimeStamp()
-		var elapsedTime = after - before
-		fmt.Printf("Elapsed Time for %d is %d\n", allInputs[i], elapsedTime)
-	}
-}
-
-func GetTimeStamp() int64 {
-	return time.Now().UnixNano() / int64(time.Millisecond)
+	var num = 100_100
+	fmt.Printf("num: %v\n", num)
 }
