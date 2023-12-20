5216cf87bf8a88ef70b14039e688d4da9c9d8668
lwenjim <liuwenjim@gmail.com>
2023-12-20 09:24:57 +0800

    feat: update jwt

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
-import "fmt"
+type Node3 interface {
+	Add(a, b int32) int32
+	Sub(a, b int64) int64
+}
+
+type SObj struct{ id int32 }
+
+func (adder SObj) Add(a, b int32) int32 { return a + b }
+func (adder SObj) Sub(a, b int64) int64 { return a - b }
 
 func main() {
-	var num = 100_100
-	fmt.Printf("num: %v\n", num)
+	m := Node3(SObj{id: 6754})
+	m.Add(10, 32)
 }
