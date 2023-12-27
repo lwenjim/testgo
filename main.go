0a2940c9471c52fbb85c195219f1829ea9d91b8b
lwenjim <liuwenjim@gmail.com>
2023-12-27 09:19:31 +0800

    feat: clean rabbish

--- main.go
+++ main.go
@@ -1,16 +1,7 @@
 package main
 
-type Node3 interface {
-	Add(a, b int32) int32
-	Sub(a, b int64) int64
-}
-
-type SObj struct{ id int32 }
-
-func (adder SObj) Add(a, b int32) int32 { return a + b }
-func (adder SObj) Sub(a, b int64) int64 { return a - b }
+import "fmt"
 
 func main() {
-	m := Node3(SObj{id: 6754})
-	m.Add(10, 32)
+	fmt.Println("hello worl")
 }
