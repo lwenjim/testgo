99b9ce29a12cd745bd4229adc4266ccbb1a3b835
lwenjim <liuwenjim@gmail.com>
2024-01-11 15:19:54 +0800

    feat: update main for hello world

--- main.go
+++ main.go
@@ -3,14 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	msg := "Greetings\nfrom\nTurkey\n"
-
-	var count int
-	for i := 0; i < len(msg); i++ {
-		if msg[i] == '\n' {
-			count++
-		}
-	}
-
-	fmt.Println(count)
+	fmt.Println("hello world")
 }
