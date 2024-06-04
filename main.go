42f51bbc322df8ea7deda3e9d9967b954e6fe63e
lwenjim <liuwenjim@gmail.com>
2024-06-05 00:11:58 +0800

    feat: c func return string

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
+// #include "mylib.h"
+import "C"
 import "fmt"
 
-// #include "mylib.h"
-// import "C"
-
 func main() {
-	fmt.Println("Hello, world!")
+	newName := C.GoString(C.print(C.CString("123")))
+	fmt.Println(newName)
+
+	var desc string
+	newName = C.GoString(C.print2(C.CString(desc), C.CString("456")))
+	fmt.Println(newName)
+
+	fmt.Println(C.GoString(C.print3()))
+
+	fmt.Println(C.GoString(C.print4()))
 }
--- mylib.c
+++ mylib.c
@@ -1,11 +1,34 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
 
-void print()
+char* print(char* str)
 {
-    for(int i=0;i<10;i++)
-    {
-        printf("%d ",i);
-    }
-    printf("\n");
+    char* name;
+    name = (char*)malloc(10);
+    strcpy(name, str);
+    return name;
 }
 
+char* print2(char* des, const char* source)
+{
+    char* r = des;
+    assert((r != NULL) && (source != NULL));
+    while ((*r++ = *source++)!= '\0');
+    return des;
+}
+
+char* print3()
+{
+    static char name[10];
+    strcpy(name, "柳松酒");
+    return name;
+}
+
+char name[100];
+char* print4()
+{
+    strcpy(name, "你好呀");
+    return name;
+}
