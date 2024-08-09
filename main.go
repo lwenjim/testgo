1204b84c154c17aa01fbd98dfa4c8d25bef84bf6
liuwenjin <liuwenjin@jspp.com>
2024-08-09 17:52:27 +0800

    feat: update resources

--- main.go
+++ main.go
@@ -1,31 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-)
-
-type Person struct {
-	Name string `json:"name"`
-	Age  NewInt `json:"age"`
-	sex  string `json:"sex"`
-}
-
-type (
-	NewInt uint32
-)
-
 func main() {
-	var data = &Person{"lwenjim", 11, "aaa"}
-	d_ref := reflect.ValueOf(*data)
-	_, ok := d_ref.Field(1).Interface().(uint32)
-	fmt.Printf("ok: %v\n", ok)
-
-	var a uint64 = 11
-	fmt.Printf("a: %T\n", a)
-	fmt.Println(reflect.TypeOf(a))
-}
-
-func equZeroValue[T int32](val T) {
 
 }
