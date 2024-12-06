fd5226253411ea1f7bdbd6a5de4d68ebd742aad1
liuwenjin <liuwenjin@jspp.cn>
2024-12-06 19:45:18 +0800

    feat: test net-security

--- main.go
+++ main.go
@@ -2,20 +2,9 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"net/http"
 )
 
 func main() {
-	resp, err := http.Get("https://www.baidu.com")
-	if err != nil {
-		fmt.Printf("err.Error(): %v\n", err.Error())
-		return
-	}
-	buff, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Printf("err.Error(): %v\n", err.Error())
-		return
-	}
-	fmt.Printf("string(buff): %v\n", string(buff))
+	a := "abc"
+	fmt.Println(a[len(a)-1:])
 }
