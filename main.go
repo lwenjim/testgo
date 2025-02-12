9f3f568856030dcda933ac32302b5e052078bff2
liuwenjin <liuwenjin@jspp.cn>
2025-02-12 19:05:27 +0800

    feat: add sm4

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
-import (
-	"jspp/testgo/cmd"
-)
+import "jspp/testgo/cmd"
 
 func main() {
+	// fmt.Println(time.Unix(1739003234, 0).Format("2006:01-02 15:04:05"))
+	// 2024-12-16 08:26:00
+	// updateTime, _ := time.ParseInLocation("2006-01-02 15:04:05", "2024-12-16 08:26:00", time.Local)
+	// fmt.Printf("updateTime: %v\n", updateTime.Unix())
 	cmd.Execute()
 }
