d4fe31e68f172d2c1f065c3cf1d2e8e3c90baa23
liuwenjin <liuwenjin@jspp.cn>
2025-04-10 17:33:46 +0800

    feat: docker mysql

---
+++ docker/mysql/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:123456@tcp(127.0.0.1:13306)/test")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	row, err := db.Query("show databases")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	data := []string{}
+	for row.Next() {
+		name := ""
+		row.Scan(&name)
+		data = append(data, name)
+	}
+	buffer, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(buffer))
+
+	for i := 0; i < 100; i++ {
+		res, err := db.Exec("insert into user values(null, ?, ?)", RandString(10), i+100)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		id, err := res.LastInsertId()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("id: %v\n", id)
+	}
+}
+
+func RandString(n int) string {
+	var letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	var (
+		letterIDBits       = 6
+		letterIDMask int64 = 1<<letterIDBits - 1
+		letterIDMax        = 63 / letterIDBits
+	)
+	var src = rand.NewSource(time.Now().UnixNano())
+	sb := strings.Builder{}
+	sb.Grow(n)
+	for i, cache, remain := n-1, src.Int63(), letterIDMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIDMax
+		}
+		if idx := cache & letterIDMask; idx < int64(len(letters)) {
+			sb.WriteByte(letters[idx])
+			i--
+		}
+		cache >>= letterIDBits
+		remain--
+	}
+	return sb.String()
+}
--- main.go
+++ main.go
@@ -1,25 +1,39 @@
 package main
 
-import "jspp/testgo/cmd"
+import (
+    "fmt"
+    "reflect"
+)
 
-// @Summary 查询权限菜单
-// @Description 查询权限菜单
-// @Accept  json
-// @Produce  json
-//
-// @Success 200 {object} BaseResp "ok"
-// @Router /api/v1/menu/my/select [get]
-func Say() {
+// 示例结构体
+type MyStruct struct{}
 
+// 值接收者方法
+func (m MyStruct) ValueMethod() {}
+
+// 指针接收者方法
+func (m *MyStruct) PointerMethod() {}
+
+// 检查结构体是否包含方法（含指针接收者）
+func HasMethods(s interface{}) bool {
+    // 获取类型和指针类型
+    typeOf := reflect.TypeOf(s)
+    ptrType := reflect.PtrTo(typeOf)
+
+    // 检查值类型和指针类型的方法总数
+    return typeOf.NumMethod()+ptrType.NumMethod() > 0
 }
 
-// @Summary 查询权限菜单
-// @Description 查询权限菜单
-// @Accept  json
-// @Produce  json
-//
-// @Success 200 {object} BaseResp "ok"
-// @Router /api/v1/menu/my/select [get]
 func main() {
-	cmd.Exec()
+    // 测试值类型
+    var s MyStruct
+    fmt.Println("MyStruct 是否有方法:", HasMethods(s)) // 输出: true
+
+    // 测试指针类型
+    var p *MyStruct
+    fmt.Println("*MyStruct 是否有方法:", HasMethods(p)) // 输出: true
+
+    // 测试无方法的结构体
+    type EmptyStruct struct{}
+    fmt.Println("EmptyStruct 是否有方法:", HasMethods(EmptyStruct{})) // 输出: false
 }
