2e750d5dba09114f950e1982f988ebea68f195b5
liuwenjin <liuwenjin@jspp.cn>
2025-04-10 17:33:46 +0800

    feat: docker mysql

--- js/Runoob.ts
+++
@@ -1,2 +0,0 @@
-let someValue: any = "[1,2]";
-console.log((someValue))
--- js/my-app/src/Welcome.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// 定义一个函数组件
-function Welcome(props) {
-    return <h1>Hello, {props.name}!</h1>;
-}
-
-export default Welcome;
