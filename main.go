466a708e6f02f554fd1c628b4a42cf271340fe87
jim <liuwenjin@jspp.cn>
2025-07-31 10:50:21 +0800

    feat: add gitchange,gitchangecontent

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	data := "\t\tabc\t"
-	fmt.Println((data))
+	str := time.Date(time.Now().Year(), time.Now().AddDate(0, 1, 0).Month(), 1, 0, 0, 0, 000, time.Local).Add(-1 * time.Second)
+	fmt.Println(str.Format("2006-01-02 15:04:05"))
+	fmt.Println(time.LoadLocation("Asia/Shanghai"))
 }
