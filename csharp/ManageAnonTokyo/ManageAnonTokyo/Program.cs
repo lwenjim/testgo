a16c7d15e6f9a4a284f19d58e2d0ae7eb5342ab5
lwenjim <779772852@qq.com>
2026-03-08 15:21:22 +0800

    feat: add url for test

--- csharp/ManageAnonTokyo/ManageAnonTokyo/Program.cs
+++ csharp/ManageAnonTokyo/ManageAnonTokyo/Program.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ManageAnonTokyo {
+    // http://localhost:8082/?execName=anontokyo_server.exe
     internal class Program {
         static async Task Main(string[] args) {
             await StartService();
