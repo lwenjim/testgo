4ddd94f694661108a54bec432417891b37ffa251
jim <v_liuwenjin@bilibili.com>
2026-03-18 14:57:17 +0800

    feat:add deploy docs

--- csharp/ManageAnonTokyo/ManageAnonTokyo/Program.cs
+++ csharp/ManageAnonTokyo/ManageAnonTokyo/Program.cs
@@ -1,36 +1,7 @@
-﻿using System;
-using System.CommandLine;
-
-namespace ManageAnonTokyo {
+﻿namespace ManageAnonTokyo {
     internal class Program {
         static int Main(string[] args) {
-            var root = new RootCommand("MyApplication");
-            var service = new Command("service", "Configure the application");
-            var daemon = new Command("daemon", "install window service");
-            var run = new Command("run", "deploy and run window service");
-            var netinfo = new Command("info", "print network infomation");
-
-            run.SetAction(async (@params) => {
-                await InstallService.StartService();
-            });
-
-            daemon.SetAction((@params) => {
-                string data = InstallService.InstallDaemon();
-                if (data.Length > 0) {
-                    Console.WriteLine(data);
-                }
-            });
-
-            netinfo.SetAction((@params) => {
-                InstallService.PrintNetInfo();
-            });
-
-            root.Subcommands.Add(service);
-            service.Subcommands.Add(daemon);
-            service.Subcommands.Add(run);
-            service.Subcommands.Add(netinfo);
-
-            return root.Parse(args).Invoke();
+            return InstallService.Run(args);
         }
     }
 }
